45b635e160c23981c6275b33850e63ad10f0bf45
Nagy Csongor <91937098+Csocso87@users.noreply.github.com>
2026-04-14 19:29:04 +0200

    Add files via upload

---
+++ backend/utils/JwtHandler.php
@@ -0,0 +1,35 @@
+<?php
+class JwtHandler {
+    private static $secret = JWT_SECRET;
+    
+    public static function encode($payload) {
+        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        
+        $payload['exp'] = time() + JWT_EXPIRY;
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode(json_encode($payload)));
+        
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, self::$secret, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+        
+        return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+    }
+    
+    public static function decode($token) {
+        $parts = explode('.', $token);
+        if (count($parts) != 3) return null;
+        
+        list($base64UrlHeader, $base64UrlPayload, $base64UrlSignature) = $parts;
+        
+        $signature = base64_decode(str_replace(['-', '_'], ['+', '/'], $base64UrlSignature));
+        $expectedSignature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, self::$secret, true);
+        
+        if (!hash_equals($signature, $expectedSignature)) return null;
+        
+        $payload = json_decode(base64_decode(str_replace(['-', '_'], ['+', '/'], $base64UrlPayload)), true);
+        if (isset($payload['exp']) && $payload['exp'] < time()) return null;
+        
+        return $payload;
+    }
+}
+?>
